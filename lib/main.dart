b096291b862c969976b866ba7fabd8dfd17a84fb
tintoll <belselios@gmail.com>
2021-02-20 05:53:54 +0900

    add bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0; // 페이지
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('복잡한 UI')),
+      body: Center(
+        child: Text(
+          '$_index 페이지',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        currentIndex: _index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: '이용서비스',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: '내정보',
+          ),
+        ],
+      ),
+    );
   }
 }
